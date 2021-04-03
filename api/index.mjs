0f11a6e65ff77c49afdd05e20162fda78d6f135b
Jordan Byrd <jabyrd3@gmail.com>
2021-04-03 00:52:02 -0400

    GET domains api route

--- api/index.mjs
+++ api/index.mjs
@@ -4,12 +4,14 @@ const db = sqlite('/root/pdns.sqlite3', { verbose: console.log });
 class API {
   constructor(){
     db.pragma('journal_mode = WAL');
+    // todo: remove this eventually
     const zone = db.prepare("INSERT INTO domains (name, type) VALUES ('example.com', 'NATIVE');");
     zone.run();
     const soa = db.prepare("INSERT INTO records (domain_id, name, content, type,ttl,prio) VALUES (1,'example.com','localhost admin.example.com 1 10380 3600 604800 3600','SOA',86400,NULL)");
     soa.run();
     const a = db.prepare("INSERT INTO records (domain_id, name, content, type,ttl,prio) VALUES (1,'example.com','192.0.2.10','A',120,NULL);")
     a.run();
+    const preparedGetDomain = db.prepare("SELECT * FROM domains;");
     this.server = new Server({
       port: 8080
     });
@@ -18,6 +20,9 @@ class API {
     });
     this.server.get('/hello/:there', (req, res) => {
       res.status('200').send(req.params.there);
+    });
+    this.server.get('/domains', (req, res) => {
+      res.status('200').send(JSON.stringify(preparedGetDomain.all()));
     });
     this.server.start();
   } 
