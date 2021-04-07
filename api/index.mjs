edeb942ff536afe90c4352d7db4c46254c0450f3
Jordan Byrd <jabyrd3@gmail.com>
2021-04-07 13:20:45 -0400

    wireup userid on records fetch

--- api/index.mjs
+++ api/index.mjs
@@ -12,10 +12,12 @@ class API {
     const user = db.prepare("INSERT INTO users (username, password) VALUES (?, ?)");
     user.run('foobar', 'password');
     user.run('demo', 'anotherpassword');
-    const zone = db.prepare("INSERT INTO domains (name, type, owner) VALUES ('example.com', 'NATIVE', 1);");
-    zone.run();
-    const soa = db.prepare("INSERT INTO records (domain_id, name, content, type,ttl,prio) VALUES (1,'example.com','localhost admin.example.com 1 10380 3600 604800 3600','SOA',86400,NULL)");
-    soa.run();
+    const zone = db.prepare("INSERT INTO domains (name, type, owner) VALUES (?, 'NATIVE', ?);");
+    zone.run('example.com', 1);
+    zone.run('anotherexample.com', 2);
+    const soa = db.prepare("INSERT INTO records (domain_id, name, content, type,ttl,prio) VALUES (?, ?,'localhost admin.example.com 1 10380 3600 604800 3600','SOA',86400,NULL)");
+    soa.run(1, 'example.com');
+    soa.run(2, 'anotherexample.com');
     const a = db.prepare("INSERT INTO records (domain_id, name, content, type,ttl,prio) VALUES (1,'example.com','192.0.2.10','A',120,NULL);")
     a.run();
     
--- api/routes/record.mjs
+++ api/routes/record.mjs
@@ -1,7 +1,8 @@
+import getUser from '../middlewares/getUser.mjs';
 export default (server, db) => {
-  const preparedGetRecords = db.prepare("SELECT * FROM records WHERE domain_id = ?;");
-  server.get('/domain/:id', (req, res) => {
-    console.log('getdomainhandler hit');
-    res.status('200').send(JSON.stringify(preparedGetRecords.all(req.params.id)));
+  // const preparedGetRecords = db.prepare("select * from records INNER JOIN domains on records.domain_id = ? WHERE domains.owner = ?;");
+  const preparedGetRecords = db.prepare("SELECT * FROM domains inner JOIN records r on r.domain_id = domains.id WHERE owner = ? AND r.domain_id = ?;");
+  server.get('/domain/:id', [getUser], (req, res) => {
+    res.status('200').send(JSON.stringify(preparedGetRecords.all(req.userid, parseInt(req.params.id, 10))));
   });
 };
