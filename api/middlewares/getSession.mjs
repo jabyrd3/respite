d6b7ffb92e462fe8556252b24e285baa4650bb2f
Jordan Byrd <jabyrd3@gmail.com>
2021-07-31 23:55:09 -0400

    count users in auth middleware and allow if none users are created tweak dev seed to create first user then immediately establish session add auth middleware to user creation endpoint, that would've been a funny hole

--- api/middlewares/getSession.mjs
+++ api/middlewares/getSession.mjs
@@ -1,7 +1,10 @@
 export default (req, res, mwExtras) => {
-  var getSession = mwExtras.db.prepare("SELECT * FROM sessions WHERE uuid = ?;");
-  const session = getSession.get(req.headers.session);
-  if(!session){
+  var getSessionStatement = mwExtras.db.prepare("SELECT * FROM sessions WHERE uuid = ?;");
+  const countUsersStatement = mwExtras.db.prepare('SELECT count(id) FROM users;')
+  const count = countUsersStatement.get()
+
+  const session = getSessionStatement.get(req.headers.session);
+  if (!session && count['count(id)'] > 0) {
     res.status(401).send('invalid session');
     res = false;
   }
